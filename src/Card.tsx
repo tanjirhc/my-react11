674fc76ee1bf66ceb2ffb4ab3f3918b80bb7275a
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-21 19:23:05 +0600

    placeholder

--- src/Card.tsx
+++ src/Card.tsx
@@ -2,7 +2,7 @@
 function Card() {
   return(
     <div className="card">
-      <img></img>
+      <img src="https://via.placeholder.com/150" alt="Profile Picture"></img>
       <h2>Tanjir Hasan</h2>
       <p>I make website and design brochure for my organization.</p>
     </div>
