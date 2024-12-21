7373e2d35abba4856d199cc707b0966843ea0958
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-21 12:09:07 +0600

    list item

--- src/Header.tsx
+++ src/Header.tsx
@@ -2,6 +2,15 @@ function Header() {
   return (
     <div>
       <h1>My Website</h1>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+      <hr />
     </div>
   );
 }
